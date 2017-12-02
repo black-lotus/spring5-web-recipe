45e78805e0e67908c80e6fc573709d337b76e67f
Romdoni Agung Purbayanto <doni@stockbit.com>
2017-12-02 23:01:13 +0700

    update repository and test the data

--- src/main/java/com/dondon/recipe/controllers/IndexController.java
+++ src/main/java/com/dondon/recipe/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package com.dondon.recipe.controllers;
 
+import com.dondon.recipe.domain.Category;
+import com.dondon.recipe.domain.UnitOfMeasure;
+import com.dondon.recipe.repositories.CategoryRepository;
+import com.dondon.recipe.repositories.UnitOfMeasureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("CAT ID is : " + categoryOptional.get().getId());
+        System.out.println("UOM ID is : " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 
