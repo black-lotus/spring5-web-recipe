b64d99a7e7db5760241242d94e49030f04ed414c
Romdoni Agung Purbayanto <doni@stockbit.com>
2017-12-03 14:02:03 +0700

    using project lombok

--- src/main/java/com/dondon/recipe/controllers/IndexController.java
+++ src/main/java/com/dondon/recipe/controllers/IndexController.java
@@ -5,6 +5,7 @@ import com.dondon.recipe.domain.UnitOfMeasure;
 import com.dondon.recipe.repositories.CategoryRepository;
 import com.dondon.recipe.repositories.UnitOfMeasureRepository;
 import com.dondon.recipe.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -24,6 +26,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
